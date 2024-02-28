0d3fd7950be281c85c0e992cd1970768aad4fb51
Yousef Wael <yousef.wael2002@gmail.com>
2024-02-28 12:06:19 +0200

    connected database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://yousefwael02:Usef2002@cluster0.jdbkywx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
