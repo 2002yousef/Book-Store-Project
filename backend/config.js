6c1d340abe8fe5202610910a720f9e14d716096f
Yousef Wael <yousef.wael2002@gmail.com>
2024-06-20 17:42:39 +0300

    Get all books

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://yousefwael02:Usef2002@cluster0.jdbkywx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://yousefwael02:Usef2002@cluster0.yvwszz4.mongodb.net/bookstore-project?retryWrites=true&w=majority&appName=Cluster0'
--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,21 @@ app.post('/books', async (req, res) => {
     }
 })
 
+// Route for get all books from database
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({message: err.message});
+    }
+})
+
 // connecting to database
 mongoose
 .connect(mongoDBURL)
