4e82d6b4b2dc28021f8c239aebc43a0cf4994bb6
Yousef Wael <yousef.wael2002@gmail.com>
2024-12-26 14:23:07 +0200

    Update a book

--- backend/index.js
+++ backend/index.js
@@ -70,6 +70,34 @@ app.get('/books/:id', async (req, res) => {
     }
 })
 
+// Route for update a book 
+app.put('/books/:id', async (req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: "Please provide all fields",
+            });
+        }
+
+        const { id } = req.params;
+
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).send({ message: "Book not found" });
+        } 
+
+        return res.status(200).send({ message : 'Book updated successfully' })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
+
 // connecting to database
 mongoose
 .connect(mongoDBURL)
