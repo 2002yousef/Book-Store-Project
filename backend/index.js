470eb8347bf4bb3556f58f1f116c97e6b53cd6d4
Yousef Wael <yousef.wael2002@gmail.com>
2024-12-26 14:49:20 +0200

    Cors POLICY

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,21 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import  booksRoute  from './routes/bookRoute.js';
+import cors from 'cors';
  
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Cors POLICY
+app.use(
+    cors({
+        origin: "https://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ['Content-Type']
+    })
+);
 
 // root directory
 app.get('/', (req, res) => {
